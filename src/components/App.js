b8350c3a2a1a2f5e76797526ed827f58bee163cc
Isaiah <IaguileraCFE@gmail.com>
2024-01-23 17:38:27 -0800

    Isaiah Fetch Lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,30 @@
-// create your App component here
+import React from "react";
+import { useEffect, useState } from "react";
+
+function App() {
+  const [dogImage, setDogImage] = useState(null);
+  
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then(r => r.json())
+      .then(data =>  setDogImage(data.message));
+        }, []);
+        
+     
+  if (!dogImage){
+    return <p>Loading...</p>
+  }
+ return(
+    <div>
+      <img src={dogImage} alt="A Random dog" />
+    </div>)
+  
+  
+ 
+    
+ 
+ 
+  
+}
+
+export default App;
